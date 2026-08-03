d56aec9f6f4b7bcf14b8b866fbe03b97d0b9c49e
bgkb8235-art <bgkb8235@gmail.com>
2026-08-03 10:58:57 +0530

    Update app_test22.py

--- app_test22.py
+++ app_test22.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+st.title("Checking the person eligible for vote or not")
+#taking the input 
+age=st.number_input("Enter your age ")
+if st.button("submit") 
+  if age >= 18
+    st.success("You are Eligible to Vote")
+else
+    st.sorry("You are not eligible to vote")
